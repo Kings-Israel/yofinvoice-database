fddd3310c2da74c25733b43a13cb46afb4b20470
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2024-03-08 07:49:45 +0300

    UPdate

--- database/migrations/2024_03_07_234706_add_columns_to_programs_table.php
+++ database/migrations/2024_03_07_234706_add_columns_to_programs_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::table('programs', function (Blueprint $table) {
             $table->bigInteger('stop_supply')->nullable();
             $table->boolean('buyer_invoice_approval_required')->default(false);
-            $table->string('collection_account')->nullable();
             $table->string('factoring_payment_account')->nullable();
         });
     }
@@ -27,7 +26,6 @@ return new class extends Migration
         Schema::table('programs', function (Blueprint $table) {
             $table->dropColumn('stop_supply');
             $table->dropColumn('buyer_invoice_approval_required');
-            $table->dropColumn('collection_account');
             $table->dropColumn('factoring_payment_account');
         });
     }
